c116466dbc3526abd136045f8503c7a5b4b4e902
kaufman79 <kaufman79@gmail.com>
2024-04-04 21:54:06 -0400

    add try & except blocks

--- Python Projects/To-Do/main.py
+++ Python Projects/To-Do/main.py
@@ -2,7 +2,6 @@ while True:
     user_action = input("type add, edit, show, complete, or exit: ")
     user_action = user_action.strip()
 
-# need to apply this method to the other elif & else lines.
     if user_action.startswith("add"):
         todo = user_action[4:]
 
@@ -24,32 +23,43 @@ while True:
             print(row)
 
     elif user_action.startswith("edit"):
-        number_edendum = int(user_action[5:])
-        print(number_edendum)
+        try:
+            number_edendum = int(user_action[5:])
+            print(number_edendum)
 
-        number_edendum = number_edendum - 1
+            number_edendum = number_edendum - 1
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-        new_todo = input("what is the altered todo? ")
-        todos[number_edendum] = new_todo + '\n'
+            new_todo = input("what is the altered todo? ")
+            todos[number_edendum] = new_todo + '\n'
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print("your command is not valid")
+            continue
 
     elif user_action.startswith("complete"):
-        number_delendum = int(user_action[9:])
+        try:
+            number_delendum = int(user_action[9:])
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-        index = number_delendum - 1
-        todo_delendum = todos[index].strip('\n')
-        todos.pop(index)
+            index = number_delendum - 1
+            todo_delendum = todos[index].strip('\n')
+            todos.pop(index)
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        except IndexError:
+            print("your command is not valid")
+            continue
+        except ValueError:
+            print("your command is not valid")
+            continue
 
     elif user_action.startswith("exit"):
         break
