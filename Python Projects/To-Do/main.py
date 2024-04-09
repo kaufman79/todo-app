c2776a78bbb2705cc326a0b1caac39ac2e6e223c
kaufman79 <kaufman79@gmail.com>
2024-04-08 21:45:31 -0400

    add parameters & doc-strings to functions

--- Python Projects/To-Do/main.py
+++ Python Projects/To-Do/main.py
@@ -1,10 +1,14 @@
-def get_todos(filepath):
+def get_todos(filepath="todos.txt"):
+    """ Read text file and return list of
+    to-do items.
+    """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="todos.txt"):
+    """ Write the to-do items into the text file."""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
@@ -16,14 +20,14 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos("todos.txt")
+        todos = get_todos()
 
         todos.append(todo + "\n")
 
-        write_todos("todos.txt", todos)
+        write_todos(todos)
 
     elif user_action.startswith("show"):
-        todos = get_todos("todos.txt")
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -37,12 +41,12 @@ while True:
 
             number_edendum = number_edendum - 1
 
-            todos = get_todos("todos.txt")
+            todos = get_todos()
 
             new_todo = input("what is the altered todo? ")
             todos[number_edendum] = new_todo + '\n'
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
 
         except ValueError:
             print("your command is not valid")
@@ -52,13 +56,13 @@ while True:
         try:
             number_delendum = int(user_action[9:])
 
-            todos = get_todos("todos.txt")
+            todos = get_todos()
 
             index = number_delendum - 1
             todo_delendum = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
 
             message = f"Todo {todo_delendum} was removed from the list."
             print(message)
