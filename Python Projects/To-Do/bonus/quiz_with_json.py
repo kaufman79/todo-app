c2a5639fa971a3b6deff361ab4c4e958ef5a9f26
kaufman79 <kaufman79@gmail.com>
2024-04-11 17:18:56 -0400

    add quiz using json data

---
+++ Python Projects/To-Do/bonus/quiz_with_json.py
@@ -0,0 +1,20 @@
+import json
+
+with open("questions.json", 'r') as file:
+    content = file.read()
+
+data = json.loads(content)
+
+score = 0
+for question in data:
+    print(question["question_text"])
+    for index, alternative in enumerate(question["alternatives"]):
+        print(index + 1, "-", alternative)
+    user_choice = int(input("Enter your answer: "))
+    question["user_choice"] = user_choice
+    if question["user_choice"] == question["correct_answer"]:
+        score = score + 1
+
+percent_score = float(score / len(data) * 100)
+print("your score is", score, "/", len(data), "which is", percent_score, "%")
+print("goodbye!")
