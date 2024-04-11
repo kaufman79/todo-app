e65c5655adc4b768bc7db2359b749c9a9850ec91
kaufman79 <kaufman79@gmail.com>
2024-04-11 18:13:34 -0400

    finish quiz which uses json data

--- Python Projects/To-Do/bonus/quiz_with_json.py
+++ Python Projects/To-Do/bonus/quiz_with_json.py
@@ -5,15 +5,25 @@ with open("questions.json", 'r') as file:
 
 data = json.loads(content)
 
-score = 0
 for question in data:
     print(question["question_text"])
     for index, alternative in enumerate(question["alternatives"]):
         print(index + 1, "-", alternative)
     user_choice = int(input("Enter your answer: "))
     question["user_choice"] = user_choice
+
+
+score = 0
+for index, question in enumerate(data):
     if question["user_choice"] == question["correct_answer"]:
         score = score + 1
+        result = "correct answer"
+    else:
+        result = "incorrect answer"
+
+    message = f"{index + 1}. Your answer: {question['user_choice']}, " \
+              f"Correct answer: {question['correct_answer']} ({result})"
+    print(message)
 
 percent_score = float(score / len(data) * 100)
 print("your score is", score, "/", len(data), "which is", percent_score, "%")
