0b75a64711062197b6e890fe0ab29d2a4a25cb57
kaufman79 <kaufman79@gmail.com>
2024-04-12 14:48:38 -0400

    add a few lines into gui

--- Python Projects/To-Do/gui.py
+++ Python Projects/To-Do/gui.py
@@ -3,7 +3,7 @@ import PySimpleGUI as sg
 
 # the argument must be a string
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="enter todo")
+input_box = sg.InputText(tooltip="enter todo", key="todo")
 add_button = sg.Button("Add")
 
 layout = [
@@ -15,9 +15,10 @@ layout = [
 # first arg is app title. layout gets a list of object instances (buttons, text boxes, etc).
 # in layout, inner square brackets list(s) is a row, thus put in separate lists for separate rows.
 # I used the cookbook for a different way to make the layout (defined above as a variable, input here)
-window = sg.Window("To-Do App", layout)
+window = sg.Window("To-Do App", layout, font=('Helvetica', 20))
 
-# this displays the window on the screen
-window.read()
+# window.read displays the window on the screen, and also returns events, i.e. the user clicking buttons and such
+event = window.read()
+print(event)
 # at this point, the program holds open til closing. So here is where we will add certain commands.
 window.close()
