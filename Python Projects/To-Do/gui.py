3f9f87c619846e54ceb9e3e9a152f1aa1cb4032a
kaufman79 <kaufman79@gmail.com>
2024-04-17 21:50:16 -0400

    implement the edit button

--- Python Projects/To-Do/gui.py
+++ Python Projects/To-Do/gui.py
@@ -1,15 +1,21 @@
 import functions
 import PySimpleGUI as sg
 
-# the argument must be a string
-label = sg.Text("Type in a to-do")
+# stuff related to adding a to_do
+label = sg.Text("Type in a to-do") # the argument must be a string
 input_box = sg.InputText(tooltip="enter todo", key="todo")
 add_button = sg.Button("Add")
+
+# existing to_do stuff
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=False, size=[45,10])
+edit_button = sg.Button("Edit")
 
 
 layout = [
             [label],
             [input_box, add_button],
+            [list_box, edit_button]
          ]
 
 # window is the mother instance that contains the objects
@@ -23,14 +29,27 @@ while True:
 # attached to an event, it also returns a dictionary, with the key being 0 by default,
 # or whatever we assign it to in the code for the input box above; the value is what the user entered.
     event, values = window.read() # yes, we can assign 2 vars to a list or tuple
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values["todo"] # this gives us the value of the key to_do, assigning it to a var
+            new_todo = values["todo"] + "\n" # this gives us the value of the key to_do, assigning it to a var
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
